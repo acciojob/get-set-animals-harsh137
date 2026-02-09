cc9b01552feb6c4041082f822007f9a7afe05d1f
Harsh gupta <48770050+harsh137@users.noreply.github.com>
2026-02-09 13:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-//complete this code
-class Animal {}
+// complete this code
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // getter
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
